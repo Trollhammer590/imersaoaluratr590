45e56d6e17f7003858807532640544d4a87f0216
Julianne Wolkmer Henrique Silva <trollhammer590@outlook.com>
2024-09-05 16:18:58 -0300

    Terceira Aula da Imersão. Versão 1.2

---
+++ dados.js
@@ -0,0 +1,33 @@
+let dados = [{
+    jogo: "Magic: The Gatering",
+    descricao: "At its core, Magic is a strategy game in which players take on the role of powerful wizards, casting spells, summoning creatures, and manipulating the battlefield to defeat their opponents. Each card represents a unique element of the game, from creatures with special abilities to powerful artifacts and spells.",
+    link: "https://magic.wizards.com/pt-BR"
+},
+
+{
+    jogo: "Yu-Gi-Oh!",
+    descricao: "Yu-Gi-Oh! is a strategic card game where players Duel each other using a Deck of cards. Players take turns summoning monsters, casting spells, and activating Traps to reduce their opponent's Life Points to zero. Each card has unique abilities and strategies, making for a diverse and exciting gameplay experience.",
+    link: "https://www.yugioh.com/en/遊戯王-official-site/"
+  },
+{
+    jogo: "Pokémon Trading Card Game",
+    descricao: "The Pokémon Trading Card Game is a collectible card game where players take on the role of Pokémon trainers, building Decks of Pokémon and Trainer cards. Players strategically use their cards to battle other trainers, aiming to knock out their opponent's Pokémon. The game features a variety of Pokémon with unique abilities and attacks, offering a fun and challenging gameplay experience for all ages.",
+    link: "https://www.pokemon.com/us/pokemon-tcg/"
+  },
+dungeonsd = {
+    jogo: "Dungeons & Dragons",
+    descricao: "Dungeons & Dragons (D&D) is a fantasy medieval tabletop role-playing game (RPG) where players assume the roles of heroes who explore dungeons, fight monsters, and complete quests. The game is known for its freedom of creation and rich history and mythology.",
+    link: "https://www.dndbeyond.com/"
+  },
+  
+blackstories = {
+    jogo: "Black Stories",
+    descricao: "Black Stories is a cooperative card game where players attempt to solve obscure mysteries through questions and answers. Each card presents an enigmatic story, and players must work together to discover what really happened.",
+    link: "https://www.blackstories.com/"
+  },
+  
+bang = {
+    jogo: "Bang!",
+    descricao: "Bang! is a bluffing and deduction card game set in the Wild West. Players take on roles such as the Sheriff, the Outlaw, the Renegade, or the Deputy, and must use their cards to shoot other players and achieve their goals.",
+    link: "https://ludopedia.com.br/jogo/bang"
+  }];
