fb50b92072db32e8b1b614ae413dd4b507e5ae39
Julianne Wolkmer Henrique Silva <trollhammer590@outlook.com>
2024-09-05 21:07:26 -0300

    Terceira Aula da Imersão. Versão 1.3

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log(dados);
+function pesquisar(){
+    let section = document.getElementById("resultados-pesquisa");
+console.log(section);
+
+let resultados = "";
+
+for (let dado of dados){
+resultados += `
+    <div class="item-resultado">
+        <h2><a href=""></a> ${dado.jogo}</a></h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a class="more-info" href="${dado.link}" target="_parent">More About ${dado.jogo}</a>
+    </div>
+`}
+
+    section.innerHTML = resultados;
+}
+
+
+
+    
